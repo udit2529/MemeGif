267cedca2d6b670920c01cc7322497000c7a6126
udit2529 <uditmaheshwari17@gmail.com>
2023-06-05 13:29:06 +0530

    done

---
+++ hooks/useGif.js
@@ -0,0 +1,32 @@
+import React,{useState,useEffect}from "react";
+import axios from "axios";
+
+
+const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
+
+const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+
+const useGif = (tag) => {
+    const [gif, setGif] = useState("");
+    const [loading,setLoading]= useState(false);
+   
+
+
+    async function fetchData() {
+        setLoading(true);
+        
+        const {data} = await axios.get(tag?`${url}` : url);
+        const imageSource= data.data.images.downsized_large.url;
+        //console.log(imageSource);
+        setGif(imageSource)
+        setLoading(false);
+      }
+    
+      useEffect(() => {
+        fetchData();
+      }, []);
+
+      return {gif,loading,fetchData}
+};
+
+export default useGif;
